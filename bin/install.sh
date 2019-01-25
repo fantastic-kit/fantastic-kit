1bec93c18e87520c573070754101cc54a3cd6a68
Azhng <archer.xn@gmail.com>
2019-01-25 15:48:38 -0500

    bootstrap config

--- bin/install.sh
+++ bin/install.sh
@@ -7,6 +7,7 @@ appendIfNotExist() {
 }
 
 kitDir=$HOME/.fantastic-kit
+kitConfigDir=$HOME/.config/fantastic-kit
 
 # in case user has already installed fk, nuke everything and reinstall
 if [[ -d $kitDir ]]; then
@@ -23,19 +24,23 @@ else
   git clone --depth=1 https://github.com/fantastic-kit/fantastic-kit.git $kitDir
 fi
 
+# setup $HOME/.config/fantastic-kit
+mkdir -p $kitConfigDir
+cp $kitDir/configs/default_config.yml $kitConfigDir/config.yml
+echo $(date +'%s') > $kitConfigDir/lastUpdated
 
 echo -e "Appending following configurations to ~/.zshrc\e[91m"
 
 appendIfNotExist "######## Setup script setup by fantastic-kit ########"
 
-# sourcing fk.sh if it is not already added to the .zshrc file
-appendIfNotExist "source \$HOME/.fantastic-kit/bin/fk.sh"
-
 # export env var for fantastic-kit itself
-appendIfNotExist "export FANTASTIC_ROOT=$kitDir"
+appendIfNotExist "export FANTASTIC_ROOT=\$HOME/.fantastic-kit"
+
+# sourcing fk.sh if it is not already added to the .zshrc file
+appendIfNotExist "source \$FANTASTIC_ROOT/bin/fk.sh"
 
 # update $PATH to include scripts needed for fk
-appendIfNotExist "export PATH=$kitDir/bin:\$PATH"
+appendIfNotExist "export PATH=\$FANTASTIC_ROOT/bin:\$PATH"
 
 # setup fpath for autocomplete function
 appendIfNotExist "fpath=(\$FANTASTIC_ROOT/autocompletion \$fpath)"
