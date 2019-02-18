6c2f2ca042352339d5e4f1468c39d94ad9463d63
Aman Arora <aman23091998@gmail.com>
2019-02-18 16:46:54 -0500

    Request feature/ Report bug

--- bin/wrappers.sh
+++ bin/wrappers.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env bash
 
+# $1 0 if feature request, else bug.
+_fk_create_issue()
+{
+    if [[ $1 -eq 0 ]]; then
+        issue_type="request a feature"
+    else
+        issue_type="report a bug"
+    fi
+
+    echo "If you wish to $issue_type, please create an issue at"
+    echo "https://github.com/fantastic-kit/fantastic-kit/issues/new"
+}
+
 _fk_wrapper_open()
 {
     case $(uname) in
@@ -11,6 +24,7 @@ _fk_wrapper_open()
             ;;
         *)
             echo "Sorry but we don't support $(uname) yet."
+            _fk_create_issue 0
             ;;
     esac
 }
