63d95402ce741cb269ecdca398bb3e84c1fda65d
Aman Arora <me@aman-arora.com>
2020-03-03 00:09:48 -0800

    Introduce fk up
    Allows user to create a repository on github from CLI
    
    Currently requires user to enter password to create the repository

--- lib/fkit/help.rb
+++ lib/fkit/help.rb
@@ -1,6 +1,7 @@
 module FKit
   HELP_MSG = <<-EOF
 Usage: fk [cmd] (subcmd|options)
+  fk \e[94mup\e[39m           -- Create a new repository on github that is tracked by fantastic-kit
   fk \e[94mcd\e[39m           -- cd into a project tracked by fantastic-kit
   fk \e[94mclone\e[39m        -- clone a repository from GitHub
   fk \e[94mpr\e[39m           -- create a GitHub pull request for current repo
